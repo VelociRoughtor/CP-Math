f3853b3d5725ce20241173270b95f80182280f75
YourUsername <mdmehedihasanrafy67865@gmail.com>
2025-06-02 19:18:14 +0600

    first commit

---
+++ Big Integer/Addition.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string sum (string &a, string &b) {
+    string c = "";
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    int car = 0, n = a.size(), m = b.size();
+    for (int i = 0; i < max(n, m); i++) {
+        int x = (i < n) ? a[i] - '0' : 0;
+        int y = (i < m) ? b[i] - '0' : 0;
+        int s = x + y + car;
+        c.push_back(s % 10 + '0');
+        car = s / 10;
+    }
+    if (car) c.push_back(car + '0');
+    reverse(c.begin(), c.end());
+    return c;
+}
+
+void solve() {
+    string a, b; 
+    cin >> a >> b;
+    cout << sum(a, b) << endl;
+}
+
+int main(){
+    int t; 
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
