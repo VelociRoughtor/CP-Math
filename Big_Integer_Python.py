533e0a95f214439e4cdf27424bd716dff339645f
YourUsername <mdmehedihasanrafy67865@gmail.com>
2025-06-02 20:37:26 +0600

    Big Integer Completed

---
+++ Big_Integer_Python.py
@@ -0,0 +1,15 @@
+a = 1122233334444444333333
+b = 9
+
+# Performing arithmetic operations on large integers
+print(a + b)
+
+print(b - a)
+
+print(a * b)
+
+print(a / b)
+
+print(a ** b)
+
+print(a % b)
---
+++ Julka.py
@@ -0,0 +1,8 @@
+def main():
+    for i in range(10):
+        a = int(input())
+        b = int(input())
+        print ((a + b) // 2)
+        print ((a - b) // 2)
+
+main()
